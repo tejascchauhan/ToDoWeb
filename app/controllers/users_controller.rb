33ac36562b5cc24bbef14784b8c6444fcb0ae4f1
Tejas Chauhan <tejas.chauhan@marwadieducation.edu.in>
2021-11-24 20:10:55 +0530

    create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
 
+  skip_before_action:verify_authenticity_token
+
   def index
     render plain: User.all.map { |user| todo.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    response_text = "Welcome #{new_user.name}, \nYour id: #{new_user.id}"
+    render plain: response_text
   end
 
 end
