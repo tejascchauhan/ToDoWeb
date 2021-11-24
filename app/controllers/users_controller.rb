60e1338ed42d8eb242421dc9d03187956217c659
Tejas Chauhan <tejas.chauhan@marwadieducation.edu.in>
2021-11-24 21:27:59 +0530

    add login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   skip_before_action:verify_authenticity_token
 
   def index
-    render plain: User.all.map { |user| todo.to_pleasant_string }.join("\n")
+    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
   end
 
   def create
@@ -12,6 +12,14 @@ class UsersController < ApplicationController
     password = params[:password]
     new_user = User.create!(name: name, email: email, password: password)
     response_text = "Welcome #{new_user.name}, \nYour id: #{new_user.id}"
+    render plain: response_text
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by(email: email, password: password)
+    response_text = user ? "true" : "false"
     render plain: response_text
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources :todos
   resources :users
+  post "users/login", to: "users#login"
 
 end
