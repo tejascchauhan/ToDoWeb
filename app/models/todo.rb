7d9618750eb3ac97702d87f36c88b93936e03547
Tejas Chauhan <tejas.chauhan@marwadieducation.edu.in>
2021-11-28 16:24:59 +0530

    add view for Todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,17 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[x]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
   end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
 end
